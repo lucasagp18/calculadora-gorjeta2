c3872edc289d222976f83fe8e0a0858fefb42884
lucasagp18 <lucasagp1804@gmail.com>
2024-03-28 12:14:10 -0300

    sexto commit do js e agora sim projeto finalizado

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ let pessoas = 0
 let porcentagem = 0
 const contaInput = document.querySelector("#conta")
 contaInput.addEventListener("input", receberValorConta)
-
 function receberValorConta(evento) {
     conta = Number(evento.target.value)
     calcular()
 }
-
 const pessoasInput = document.querySelector("#pessoas")
 pessoasInput.addEventListener("input", receberQuantidadePessoas)
 function receberQuantidadePessoas(evento) {
@@ -22,10 +20,8 @@ function receberQuantidadePessoas(evento) {
         divErro.setAttribute("id", "")
         pessoas = Number(evento.target.value)
     }
-
     calcular()
 }
-
 const botoesGorjeta = document.querySelectorAll(".gorjeta input[type='button']")
 botoesGorjeta.forEach(botao => {
     botao.addEventListener("click", receberPorcentagem)
@@ -44,34 +40,30 @@ function receberPorcentagem(evento) {
     }
     calcular()
 }
-
 const gorjetaInput = document.querySelector("#outra")
 gorjetaInput.addEventListener("input", receberPorcentagem)
-
 function calcular() {
     if(conta !== 0 && porcentagem !== 0 && pessoas !== 0) {
         const strongGorjetaTotal = document.querySelector(".gorjeta-total > strong")
         strongGorjetaTotal.innerHTML = `R$ ${(conta * porcentagem / pessoas).toFixed(2)}`
-
         const strongTotal = document.querySelector(".total > strong")
         strongTotal.innerHTML = `R$ ${((conta + (conta * porcentagem)) / pessoas).toFixed(2)}`
     }
 }
-
 const botaoLimpar = document.querySelector(".resultados button")
 botaoLimpar.addEventListener("click", limpar)
-
 function limpar() {
     contaInput.value = ""
-
     botoesGorjeta.forEach(botao => {
         botao.classList.remove("botao-ativo")
     })
-
     gorjetaInput.value = ""
-
     pessoasInput.value = ""
 
     document.querySelector(".gorjeta-total > strong").innerHTML = "R$ 0.00"
     document.querySelector(".total > strong").innerHTML = "R$ 0.00"
+
+    conta = 0
+    porcentagem = 0
+    pessoas = 0
 }
